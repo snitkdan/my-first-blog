8119c10682e9f24a59ec407073e970ad25421e16
Daniel <snitkdan@uw.edu>
2016-08-01 22:28:01 +0000

    remove settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-	'NAME': 'blogapp',
-	'USER': 'snitkdan',
-	'PASSWORD': 'Daniel27876',
-	'HOST': 'localhost',
-	'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+	'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
